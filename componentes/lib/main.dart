5a16ce7ca11638fcfac9965bcf2692c52f13de42
AngelCruz <37281529+angelcruzalv@users.noreply.github.com>
2020-06-03 16:50:51 -0500

    archivo rutas
    Obtenemos las rutas desde un archivo dedicado a las rutas.

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
-import 'package:componentes/src/pages/home_temp.dart';
+
+import 'package:componentes/src/routes/routes.dart';
 import 'package:flutter/material.dart';
 
 import 'src/pages/home_page.dart';
@@ -15,11 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       // home:  HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/'      : (BuildContext context) => HomePage(),
-        'alert'  : (BuildContext context) => AlertPage(),
-        'avatar' : (BuildContext context) => AvatarPage(),
-      }
+      routes: getApplicationRoutes(),
     );
   }
 }
