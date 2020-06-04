2174c61060f73fb4b4cb9705b8f06325258f4292
AngelCruz <37281529+angelcruzalv@users.noreply.github.com>
2020-06-03 22:57:33 -0500

    Alert Page
    Se agregó botón que activa la alerta

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -9,6 +9,44 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed:()=> _mostrarAlert(context),
+        ),
+      ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        title: Text('Alert Dialog'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Este es el contenido de la Alerta'),
+            FlutterLogo(size: 100,),
+          ],
+      
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Aceptar'),
+            onPressed: ()=> Navigator.of(context).pop(), 
+          ),
+          FlatButton(
+            onPressed: ()=>Navigator.of(context).pop(),
+            child: Icon(Icons.cancel))
+        ],
+      ),
     );
   }
 }
