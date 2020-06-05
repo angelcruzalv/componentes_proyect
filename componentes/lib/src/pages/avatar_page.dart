678c758330229b04349faa221bbc56533afb521e
AngelCruz <37281529+angelcruzalv@users.noreply.github.com>
2020-06-05 15:52:42 -0500

    Animated Container
    Se creó la nueva PAGE ANIMATEDCONTAINER, con las respectivas modificaciones que esta representa en el código general de la App.

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -8,6 +8,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(6.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://i0.wp.com/espacionegocios.com.ar/wp-content/uploads/2020/02/elon-musk.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('EM'),
+              backgroundColor: Colors.purple,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://estaticos.elperiodico.com/resources/jpg/3/2/elon-musk-suma-mas-problemas-1538084324023.jpg'),
+          fadeInDuration: Duration(milliseconds: 2000),
+        ),
       ),
     );
   }
